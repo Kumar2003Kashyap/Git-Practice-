901cba7b503380ff7032f97422bf8c9d93dff559
kk <kumarkashyaponly@gmail.com>
2026-03-03 11:49:21 +0530

    service added

--- hello.java
+++ hello.java
@@ -1,5 +1,14 @@
 public class hello {
     public static void main(String[] args) {
         System.out.println("hello");
+
+      
+    }
+      public void print(){
+            System.out.println("hello j");
+        }
+
+    public void msg(){
+        System.out.println("msg user");
     }
 }
